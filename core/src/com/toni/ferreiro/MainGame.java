a20e5da9e1c26c7bd933d0903fd7f92c7e4ab421
toni <ferreirocouto@gmail.com>
2021-02-02 12:00:08 +0100

    probando texture region

--- core/src/com/toni/ferreiro/MainGame.java
+++ core/src/com/toni/ferreiro/MainGame.java
@@ -5,10 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MainGame extends ApplicationAdapter {
 
-	private Texture player1;
+	private Texture player1, pincho;
+	private TextureRegion regionPinchos;
 
 	private SpriteBatch batch;
 
@@ -19,7 +21,10 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		player1 = new Texture("player1.png");
+		pincho = new Texture("spike.png");
+
 		batch = new SpriteBatch();
+		regionPinchos = new TextureRegion(pincho, 80, 80);
 
 		System.out.println("Pantalla: "+ Gdx.graphics.getWidth()+ " x "+Gdx.graphics.getHeight());
 		width = Gdx.graphics.getWidth();
@@ -41,12 +46,16 @@ public class MainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0 , 0.5f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		//se puede dibujar fuera de batch pero es necesario bind y otras herramientas
+
 		batch.begin();
-		batch.draw(player1, 0, 0);
-		batch.draw(player1, width -widhtPlayer1, 0);
-		batch.draw(player1, 0, height -heightPlayer1);
-		batch.draw(player1, width - widhtPlayer1, height - heightPlayer1);
-		batch.draw(player1, 100, 100, 300, 250);
+		batch.draw(player1, 50, 0);
+//		batch.draw(player1, width -widhtPlayer1, 0);
+//		batch.draw(player1, 0, height -heightPlayer1);
+//		batch.draw(player1, width - widhtPlayer1, height - heightPlayer1);
+//		batch.draw(player1, 100, 100, 300, 250);
+
+		batch.draw(regionPinchos, 250, 0);
 		batch.end();
 	}
 }
