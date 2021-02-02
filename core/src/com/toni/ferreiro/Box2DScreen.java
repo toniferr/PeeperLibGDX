54c2b782e27f6caa2bacbb62b008c0c11fd27505
toni <ferreirocouto@gmail.com>
2021-02-02 17:54:35 +0100

    creacion camera

--- core/src/com/toni/ferreiro/Box2DScreen.java
+++ core/src/com/toni/ferreiro/Box2DScreen.java
@@ -2,7 +2,9 @@ package com.toni.ferreiro;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Box2DScreen extends BaseScreen{
@@ -13,14 +15,21 @@ public class Box2DScreen extends BaseScreen{
 
     private World world;
 
+    private Box2DDebugRenderer renderer;
+
+    private OrthographicCamera camera;
+
     @Override
     public void show() {
         world = new World(new Vector2(0, -10), true); //en la tierra es -9.8
+        renderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override
     public void dispose() {
         world.dispose();
+        renderer.dispose();
     }
 
     @Override
@@ -28,5 +37,8 @@ public class Box2DScreen extends BaseScreen{
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         world.step(delta, 6, 2);
+
+        camera.update();
+        renderer.render(world, camera.combined);
     }
 }
